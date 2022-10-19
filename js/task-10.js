f48ac37c3f339135f203e9a73819a32f94632449
Maksym Shvets <maxim27061993@outlook.com>
2022-10-19 18:11:11 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const numberRef = document.querySelector("input");
+const btnsRef = document.querySelectorAll("button");
+const btnCreateRef = btnsRef[0];
+const btnClearRef = btnsRef[1];
+numberRef.addEventListener("change", (event) => {
+
+});
+console.log(number);
